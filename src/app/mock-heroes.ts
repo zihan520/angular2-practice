8413302f7a4933ddc97c6819d05e05106724ef6f
admin <1145131890@qq.com>
2018-03-28 17:46:34 +0800

    hero_component过度到hero_detail_component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES:Hero[] = [
+	{ id:11,name:'Mr.Nice' },
+	{ id:12,name:'Mike' },
+	{ id:13,name:'Bottom' },
+	{ id:14,name:'Dr IQ' },
+	{ id:15,name:'Mr.Nice' },
+	{ id:16,name:'Ajax' },
+	{ id:17,name:'TypeScript' },
+	{ id:18,name:'Tornado' },
+	{ id:19,name:'Dynamic' },
+	{ id:20,name:'Javascript' },
+]
